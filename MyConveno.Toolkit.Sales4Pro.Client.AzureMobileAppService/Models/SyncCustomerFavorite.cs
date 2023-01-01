0d358e329a7e2f0b21393c0ca2a3211d75671409
Michael Coelsch <michael.coelsch@myconveno.de>
2023-01-01 12:49:54 +0100

    mc

--- MyConveno.Toolkit.Sales4Pro.Client.AzureMobileAppService/Models/SyncCustomerFavorite.cs
+++ MyConveno.Toolkit.Sales4Pro.Client.AzureMobileAppService/Models/SyncCustomerFavorite.cs
@@ -3,7 +3,7 @@ using System;
 
 namespace MyConveno.Toolkit.Sales4Pro.Client.AzureMobileAppService;
 
-abstract class SyncCustomerFavorite : DatasyncClientData, IEquatable<SyncCustomerFavorite>
+public class SyncCustomerFavorite : DatasyncClientData, IEquatable<SyncCustomerFavorite>
 {
     public string? UserID { get; set; }
     public string? CustomerNumber { get; set; }
