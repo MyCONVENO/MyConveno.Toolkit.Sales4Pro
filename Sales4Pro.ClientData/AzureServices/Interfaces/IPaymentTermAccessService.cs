57b07e5a11b96b52654707e19c963e6f40be1af8
Michael Coelsch <michael.coelsch@myconveno.de>
2023-03-31 17:40:03 +0200

    mc

--- Sales4Pro.ClientData/AzureServices/Interfaces/IPaymentTermAccessService.cs
+++ Sales4Pro.ClientData/AzureServices/Interfaces/IPaymentTermAccessService.cs
@@ -4,6 +4,7 @@ public interface IPaymentTermAccessService
 {
     Task<bool> AddPaymentTerm(PaymentTerm paymentTerm);
     Task<bool> DeletePaymentTerm(string paymentTermid);
+    Task<bool> DeleteAllPaymentTerms();
     Task<List<PaymentTerm>> GetAllPaymentTermsAsync();
     void Initialize();
     Task<bool> UpdatePaymentTerm(PaymentTerm paymentTerm);
