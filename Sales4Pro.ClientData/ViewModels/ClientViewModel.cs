e8e2054ac256193583204dcadcf7edc3e51056bb
Michael Coelsch <michael.coelsch@myconveno.de>
2023-04-02 11:55:02 +0200

    mc

--- Sales4Pro.ClientData/ViewModels/ClientViewModel.cs
+++ Sales4Pro.ClientData/ViewModels/ClientViewModel.cs
@@ -20,15 +20,176 @@ public partial class ClientViewModel : ObservableObject
     [ObservableProperty]
     public MetadataClientContent clientContent = new();
 
+
+
+    [ObservableProperty]
+    public string defaultSeason = string.Empty;
+
+    [ObservableProperty]
+    public string supportPersonName = string.Empty;
+
+    [ObservableProperty]
+    public string supportPersonEmail = string.Empty;
+
+    [ObservableProperty]
+    public string supportPersonPhone = string.Empty;
+
+    [ObservableProperty]
+    public string imagePathString = string.Empty;
+
+    [ObservableProperty]
+    public string accentColorString = string.Empty;
+
+    [ObservableProperty]
+    public string accentColorLight1String = string.Empty;
+
+    [ObservableProperty]
+    public string accentColorLight2String = string.Empty;
+
+    [ObservableProperty]
+    public string accentColorLight3String = string.Empty;
+
+    [ObservableProperty]
+    public string accentColorDark1String = string.Empty;
+
+    [ObservableProperty]
+    public string accentColorDark2String = string.Empty;
+
+    [ObservableProperty]
+    public string accentColorDark3String = string.Empty;
+
+
+    [ObservableProperty]
+    public string addressName1 = string.Empty;
+
+    [ObservableProperty]
+    public string addressName2 = string.Empty;
+
+    [ObservableProperty]
+    public string addressStreet = string.Empty;
+
+    [ObservableProperty]
+    public string addressZip = string.Empty;
+
+    [ObservableProperty]
+    public string addressCity = string.Empty;
+
+    [ObservableProperty]
+    public string addressCountryCode = string.Empty;
+
+    [ObservableProperty]
+    public string addressCountryName = string.Empty;
+
+
+    [ObservableProperty]
+    public string addressline = string.Empty;
+
+    [ObservableProperty]
+    public string addressLine1 = string.Empty;
+
+    [ObservableProperty]
+    public string addressLine2 = string.Empty;
+
+    [ObservableProperty]
+    public string addressLine3 = string.Empty;
+
+    [ObservableProperty]
+    public string addressLine4 = string.Empty;
+
+    [ObservableProperty]
+    public string addressLine5 = string.Empty;
+
+
+    [ObservableProperty]
+    public string footerLeftHeader = string.Empty;
+
+    [ObservableProperty]
+    public string footerLeftLine1 = string.Empty;
+
+    [ObservableProperty]
+    public string footerLeftLine2 = string.Empty;
+
+    [ObservableProperty]
+    public string footerLeftLine3 = string.Empty;
+
+    [ObservableProperty]
+    public string footerLeftLine4 = string.Empty;
+
+    [ObservableProperty]
+    public string footerLeftLine5 = string.Empty;
+
+
+    [ObservableProperty]
+    public string footerCenterHeader = string.Empty;
+
+    [ObservableProperty]
+    public string footerCenterLine1 = string.Empty;
+
+    [ObservableProperty]
+    public string footerCenterLine2 = string.Empty;
+
+    [ObservableProperty]
+    public string footerCenterLine3 = string.Empty;
+
+    [ObservableProperty]
+    public string footerCenterLine4 = string.Empty;
+
+    [ObservableProperty]
+    public string footerCenterLine5 = string.Empty;
+
+
+    [ObservableProperty]
+    public string footerRightHeader = string.Empty;
+
+    [ObservableProperty]
+    public string footerRightLine1 = string.Empty;
+
+    [ObservableProperty]
+    public string footerRightLine2 = string.Empty;
+
+    [ObservableProperty]
+    public string footerRightLine3 = string.Empty;
+
+    [ObservableProperty]
+    public string footerRightLine4 = string.Empty;
+
+    [ObservableProperty]
+    public string footerRightLine5 = string.Empty;
+
+
+    [ObservableProperty]
+    public string aEBRemarkText1 = string.Empty;
+
+    [ObservableProperty]
+    public string aEBRemarkText2 = string.Empty;
+
+    [ObservableProperty]
+    public string aEBRemarkText3 = string.Empty;
+
+    [ObservableProperty]
+    public string aEBRemarkText4 = string.Empty;
+
     #endregion
 
     #region Computed Properties
+
+    public bool ComputeIsPrimaryButtonEnabled
+    {
+        get
+        {
+            if (string.IsNullOrEmpty(ClientId) ||
+                string.IsNullOrEmpty(ClientName))
+                return false;
+            else
+                return true;
+        }
+    }
 
     public string ComputeZIPCity
     {
         get
         {
-            return string.Format("{0} {1}", ClientContent.Reports.AddressZip, ClientContent.Reports.AddressCity);
+            return string.Format("{0} {1}", AddressZip, AddressCity);
         }
     }
 
@@ -41,12 +202,118 @@ public partial class ClientViewModel : ObservableObject
             ClientId = string.Empty;
             ClientName = string.Empty;
 
+            DefaultSeason = string.Empty;
+            SupportPersonName = string.Empty;
+            SupportPersonEmail = string.Empty;
+            SupportPersonPhone = string.Empty;
+            ImagePathString = string.Empty;
+            AccentColorString = string.Empty;
+            AccentColorLight1String = string.Empty;
+            AccentColorLight2String = string.Empty;
+            AccentColorLight3String = string.Empty;
+            AccentColorDark1String = string.Empty;
+            AccentColorDark2String = string.Empty;
+            AccentColorDark3String = string.Empty;
+
+            AddressName1 = string.Empty;
+            AddressName2 = string.Empty;
+            AddressStreet = string.Empty;
+            AddressZip = string.Empty;
+            AddressCity = string.Empty;
+            AddressCountryName = string.Empty;
+            AddressCountryCode = string.Empty;
+            AddressLine1 = string.Empty;
+            AddressLine2 = string.Empty;
+            AddressLine3 = string.Empty;
+            AddressLine4 = string.Empty;
+            AddressLine5 = string.Empty;
+
+            FooterLeftHeader = string.Empty;
+            FooterLeftLine1 = string.Empty;
+            FooterLeftLine2 = string.Empty;
+            FooterLeftLine3 = string.Empty;
+            FooterLeftLine4 = string.Empty;
+            FooterLeftLine5 = string.Empty;
+
+            FooterCenterHeader = string.Empty;
+            FooterCenterLine1 = string.Empty;
+            FooterCenterLine2 = string.Empty;
+            FooterCenterLine3 = string.Empty;
+            FooterCenterLine4 = string.Empty;
+            FooterCenterLine5 = string.Empty;
+
+            FooterRightHeader = string.Empty;
+            FooterRightLine1 = string.Empty;
+            FooterRightLine2 = string.Empty;
+            FooterRightLine3 = string.Empty;
+            FooterRightLine4 = string.Empty;
+            FooterRightLine5 = string.Empty;
+
+            Addressline = string.Empty;
+            AEBRemarkText1 = string.Empty;
+            AEBRemarkText2 = string.Empty;
+            AEBRemarkText3 = string.Empty;
+            AEBRemarkText4 = string.Empty;
+
             ClientContent = new MetadataClientContent();
         }
         else
         {
             ClientId = client.ClientId;
             ClientName = client.ClientName;
+
+            DefaultSeason = client.MetadataContent.App.DefaultSeason;
+            SupportPersonName = client.MetadataContent.App.SupportPersonName;
+            SupportPersonEmail = client.MetadataContent.App.SupportPersonEmail;
+            SupportPersonPhone = client.MetadataContent.App.SupportPersonPhone;
+            ImagePathString = client.MetadataContent.App.ImagePathString;
+            AccentColorString = client.MetadataContent.App.AccentColorString;
+            AccentColorLight1String = client.MetadataContent.App.AccentColorLight1String;
+            AccentColorLight2String = client.MetadataContent.App.AccentColorLight2String;
+            AccentColorLight3String = client.MetadataContent.App.AccentColorLight3String;
+            AccentColorDark1String = client.MetadataContent.App.AccentColorDark1String;
+            AccentColorDark2String = client.MetadataContent.App.AccentColorDark2String;
+            AccentColorDark3String = client.MetadataContent.App.AccentColorDark3String;
+
+            AddressName1 = client.MetadataContent.Reports.AddressName1;
+            AddressName2 = client.MetadataContent.Reports.AddressName2;
+            AddressStreet = client.MetadataContent.Reports.AddressStreet;
+            AddressZip = client.MetadataContent.Reports.AddressZip;
+            AddressCity = client.MetadataContent.Reports.AddressCity;
+            AddressCountryName = client.MetadataContent.Reports.AddressCountryName;
+            AddressCountryCode = client.MetadataContent.Reports.AddressCountryCode;
+            AddressLine1 = client.MetadataContent.Reports.AddressLine1;
+            AddressLine2 = client.MetadataContent.Reports.AddressLine2;
+            AddressLine3 = client.MetadataContent.Reports.AddressLine3;
+            AddressLine4 = client.MetadataContent.Reports.AddressLine4;
+            AddressLine5 = client.MetadataContent.Reports.AddressLine5;
+
+            FooterLeftHeader = client.MetadataContent.Reports.FooterLeftHeader;
+            FooterLeftLine1 = client.MetadataContent.Reports.FooterLeftLine1;
+            FooterLeftLine2 = client.MetadataContent.Reports.FooterLeftLine2;
+            FooterLeftLine3 = client.MetadataContent.Reports.FooterLeftLine3;
+            FooterLeftLine4 = client.MetadataContent.Reports.FooterLeftLine4;
+            FooterLeftLine5 = client.MetadataContent.Reports.FooterLeftLine5;
+
+            FooterCenterHeader = client.MetadataContent.Reports.FooterCenterHeader;
+            FooterCenterLine1 = client.MetadataContent.Reports.FooterCenterLine1;
+            FooterCenterLine2 = client.MetadataContent.Reports.FooterCenterLine2;
+            FooterCenterLine3 = client.MetadataContent.Reports.FooterCenterLine3;
+            FooterCenterLine4 = client.MetadataContent.Reports.FooterCenterLine4;
+            FooterCenterLine5 = client.MetadataContent.Reports.FooterCenterLine5;
+
+            FooterRightHeader = client.MetadataContent.Reports.FooterRightHeader;
+            FooterRightLine1 = client.MetadataContent.Reports.FooterRightLine1;
+            FooterRightLine2 = client.MetadataContent.Reports.FooterRightLine2;
+            FooterRightLine3 = client.MetadataContent.Reports.FooterRightLine3;
+            FooterRightLine4 = client.MetadataContent.Reports.FooterRightLine4;
+            FooterRightLine5 = client.MetadataContent.Reports.FooterRightLine5;
+
+            Addressline = client.MetadataContent.Reports.Addressline;
+            AEBRemarkText1 = client.MetadataContent.Reports.AEBRemarkText1;
+            AEBRemarkText2 = client.MetadataContent.Reports.AEBRemarkText2;
+            AEBRemarkText3 = client.MetadataContent.Reports.AEBRemarkText3;
+            AEBRemarkText4 = client.MetadataContent.Reports.AEBRemarkText4;
 
             Client tempClient = new() { Metadata = client.Metadata };
             tempClient.DeserializeMetadata();
