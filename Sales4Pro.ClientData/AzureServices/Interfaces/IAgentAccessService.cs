12732a21bbdc0908577ce0f78baab46403af2c17
Michael Coelsch <michael.coelsch@myconveno.de>
2023-05-04 09:23:12 +0200

    mc

--- Sales4Pro.ClientData/AzureServices/Interfaces/IAgentAccessService.cs
+++ Sales4Pro.ClientData/AzureServices/Interfaces/IAgentAccessService.cs
@@ -4,6 +4,7 @@ public interface IAgentAccessService
 {
     Task<bool> AddAgent(Agent agent);
     Task<bool> DeleteAgent(string agentid);
+    Task<bool> DeleteAllAgents();
     Task<List<Agent>> GetAllAgentsAsync();
     void Initialize();
     Task<bool> UpdateAgent(Agent agent);
