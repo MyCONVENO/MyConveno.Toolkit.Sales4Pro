cff04ff78840665628097dad8ccc3583e9c1f330
Michael Coelsch <michael.coelsch@myconveno.de>
2023-08-24 14:14:15 +0200

    mc

---
+++ MyConveno.Toolkit.Sales4Pro.Client.ClientDataNET/Interfaces/ILocalFileAccessPlugIn.cs
@@ -0,0 +1,8 @@
+﻿namespace MyConveno.Toolkit.Sales4Pro.Client.ClientData;
+
+public interface ILocalFileAccessPlugIn
+{
+    Task SaveObjectToJSONFileAsync(string jsonfilename, object content);
+
+    Task<T> GetObjectFromJSONFileAsync<T>(string jsonfilename);
+}
