4eb010e16c4a111080d5196295936acd88758060
Michael Coelsch <michael.coelsch@myconveno.de>
2024-03-07 15:42:49 +0100

    mc

--- Sales4Pro.WinUI.CustomControls/CustomControls/PageSmallHeaderTextControl.cs
+++ Sales4Pro.WinUI.CustomControls/CustomControls/PageSmallHeaderTextControl.cs
@@ -26,4 +26,15 @@ public sealed class PageSmallHeaderTextControl : Control
     public static readonly DependencyProperty TitleProperty =
         DependencyProperty.Register("Title", typeof(string), typeof(PageSmallHeaderTextControl), new PropertyMetadata("Title"));
 
+
+    public string Text
+    {
+        get { return (string)GetValue(TextProperty); }
+        set { SetValue(TextProperty, value); }
+    }
+    
+    // Using a DependencyProperty as the backing store for Header.  This enables animation, styling, binding, etc...
+    public static readonly DependencyProperty TextProperty =
+        DependencyProperty.Register("Text", typeof(string), typeof(PageSmallHeaderTextControl), new PropertyMetadata("Text"));
+
 }
